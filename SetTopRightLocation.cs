690cc99e2012ad8d49acf02a57138171b03ee56b
TimeSprinkler <brandonjgutowski@gmail.com>
2016-07-29 21:07:27 -0400

    Delete SetTopRightLocation.cs

--- SetTopRightLocation.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class SetTopRightLocation : MonoBehaviour {
-
-	void Awake(){
-		Camera.main.GetComponent<CameraBounds>().mTopRightTransform = this.transform;
-	}
-}
